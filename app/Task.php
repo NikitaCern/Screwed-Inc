41b8d582909ad36f1636b0286ad530664ccdc645
Niks Derums <niks.derums@gmail.com>
2020-06-07 11:22:05 +0300

    Add FK relationship methods to Task model

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    public function order() { // FK relationship
+        return $this->belongsTo('App\Order');
+    }
+    public function part() { // FK relationship
+        return $this->belongsTo('App\Part');
+    }
+    public function responsible_employee() { // FK relationship
+        return $this->belongsTo('App\User');
+    }
 }
